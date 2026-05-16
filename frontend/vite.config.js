57adc7f54d0355ce9f5fa9cd0e0a4c27c5293aa5
omarzguit0 <omarzguit0@gmail.com>
2026-05-16 22:52:01 +0100

    Enhance SEO performance and site visibility

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,36 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import sitemap from "vite-plugin-sitemap";
+
+import viteImagemin from "vite-plugin-imagemin";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
+
+    sitemap({
+      hostname: "https://mayasol.com",
+    }),
+
+    viteImagemin({
+      mozjpeg: {
+        quality: 75,
+      },
+
+      pngquant: {
+        quality: [0.7, 0.8],
+      },
+
+      svgo: {
+        plugins: [
+          {
+            removeViewBox: false,
+          },
+        ],
+      },
+    }),
   ],
-})
+});
